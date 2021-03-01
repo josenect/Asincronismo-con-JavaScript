147cdcf5ba4ed7f17e43ecd5458e67ebbce80c46
Jose Rodriguez <josenect@hotmail.es>
2021-03-01 16:38:20 -0500

    Estructura Callback

---
+++ src/callback/index.js
@@ -0,0 +1,29 @@
+function sum(num1,num2){
+
+    return num1 + num2
+}
+
+function calc(num1,num2 ,callback){
+    
+    return callback(num1 , num2)
+}
+
+console.log(calc(2,2,sum))
+console.log("soy el segundo")
+
+/// segundo callback
+function date(callback){
+
+    console.log(new Date)
+    setTimeout(() => {
+        let date = new Date
+        callback(date)
+    }, 3000);
+
+}
+
+function printDate(dateNow){
+    console.log(dateNow)
+}
+
+date(printDate)
