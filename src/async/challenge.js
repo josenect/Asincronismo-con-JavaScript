8782901257d36c60322d70e647836e4875aaefd1
Jose Rodriguez <josenect@hotmail.es>
2021-04-26 18:59:13 -0500

    Consumiendo Api con AsyncAwait

---
+++ src/async/challenge.js
@@ -0,0 +1,20 @@
+const fetchData = require('../utils/fetchData'); // importa el modulo creado en utils
+let API = 'https://rickandmortyapi.com/api/character/';
+
+
+
+const doSomething = async (url_api)=> {
+    try{
+        const data = await fetchData(url_api); // hasta que no termine la promesa no continua en ese scope 
+        console.log(data.info.count)
+        const personaje = await fetchData(url_api+data.results[0].id);
+        console.log(personaje.name)
+        const personajeOrigin = await fetchData(personaje.origin.url);
+        console.log(personajeOrigin.dimension)
+
+    }catch(error){
+        console.log(error)   //  imprime algun error de alguna promesa
+     }
+    
+    }
+    doSomething(API)
